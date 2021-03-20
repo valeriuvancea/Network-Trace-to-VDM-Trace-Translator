e5b3c3521229be8aa1f4e552d97012d8c626dfb0
Laurynas <laurynas.ubys@gmail.com>
2021-03-20 16:54:10 +0100

    Extract contents field from the message. Added configuraiton.

--- network/packetDataExtractor/http/get/mockupHTTPGetRequestPacketDataExtractor.py
+++ network/packetDataExtractor/http/get/mockupHTTPGetRequestPacketDataExtractor.py
@@ -1,9 +1,11 @@
 from network.packetDataExtractor.http.get.httpGetRequestPacketDataExtractor import HTTPGetRequestPacketDataExtractor
 from network.networkSniffer import NetworkSniffer
 from globalConfiguration import GlobalConfiguration
+import json
 
 class MockupHTTPGetRequestPacketDataExtractor(HTTPGetRequestPacketDataExtractor):
     def addAdditionalParametersToData(self):
         keyVaultIp = GlobalConfiguration.dictionary["keyVaultIp"]
+        self.data = json.loads(self.data["contents"])
         self.data["isForKeyVault"] = True if self.destination == keyVaultIp else False
         self.data["isBetweenControllers"] = True if self.destination != keyVaultIp and self.source != keyVaultIp else False
